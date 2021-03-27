dd53671f812e23786fa1a62ed13ec2c44e80b738
Nguyen Huu Thinh <54807522+thinhnguyen17596@users.noreply.github.com>
2021-03-27 16:42:35 +0700

    Update scheduled_reminders.py
    change send email

--- models/scheduled_reminders.py
+++ models/scheduled_reminders.py
@@ -1,6 +1,7 @@
 from odoo import api, models, fields
 from odoo.exceptions import ValidationError
-from datetime import datetime
+import datetime
+from datetime import timedelta
 
 
 class ScheduledReminders(models.Model):
@@ -8,28 +9,38 @@ class ScheduledReminders(models.Model):
 	_inherit = ['mail.thread', 'mail.activity.mixin']
 	_description = 'Reminders Record'
 	
-	name = fields.Char(string="Tên lời nhắc")
+	name = fields.Char('Thu')
 	
 	recipients = fields.Many2many(string="Người nhận", comodel_name='hr.employee')
 	description = fields.Text(string="Lời nhắn")
 	user_id = fields.Many2one('res.users')
-	email_recipients = fields.Char(string="Email người nhận", related="recipients.work_email")
-	name_recipients = fields.Char(string="Tên người nhận", related="recipients.name")
 	time_reminder = fields.Datetime(string='Thời gian định gửi')
 	
 	
-	# @api.depends('recipients')
-	# def _get_name(self):
-	# 	val = []
-	# 	for rec in self.recipients:
-	# 		val.append( rec.work_email)
-	# 	self.thu_recipients = val
-	
 	# Sending Email in Button Click
+	@api.multi
 	def action_send_email(self):
-		# sending the email recipients via email
+		# sending the email recipients via email:
 		template_id = self.env.ref('scheduled_reminders.reminder_email_template').id
 		template = self.env['mail.template'].browse(template_id)
 		template.send_mail(self.id, force_send=True)
+		for rec in self:
+			rec.state = 'confirm'
+			return {
+				'effect': {
+					'fadeout': 'slow',
+					'message': 'Send mail sucsses...~_~',
+					'type': 'rainbow_man',
+				}
+			}
+		
+
 	
+	def send_email(self):
+		email = self.env['scheduled.reminders'].search([])
+		for rc in email:
+			if datetime.datetime.today() >= rc.time_reminder:
+				print('chao1')
+				rc.action_send_email()
+
 
